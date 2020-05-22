fe1faeb07980b40518a7b014486a3ce7dd28f385
rebornman <fm_reborn@hotmail.com>
2020-05-22 18:42:00 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Facial_Recognition_Sandbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.originalPicture = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.processedImage = new System.Windows.Forms.PictureBox();
+            this.btnSelectPhoto = new System.Windows.Forms.Button();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.selectImageFile = new System.Windows.Forms.OpenFileDialog();
+            this.InputFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.originalPicture)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.processedImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.originalPicture);
+            this.groupBox1.Location = new System.Drawing.Point(25, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(428, 320);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Original Image";
+            // 
+            // originalPicture
+            // 
+            this.originalPicture.Location = new System.Drawing.Point(6, 19);
+            this.originalPicture.Name = "originalPicture";
+            this.originalPicture.Size = new System.Drawing.Size(416, 295);
+            this.originalPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.originalPicture.TabIndex = 0;
+            this.originalPicture.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.processedImage);
+            this.groupBox2.Location = new System.Drawing.Point(483, 32);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(428, 320);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Processed Image";
+            // 
+            // processedImage
+            // 
+            this.processedImage.Location = new System.Drawing.Point(6, 19);
+            this.processedImage.Name = "processedImage";
+            this.processedImage.Size = new System.Drawing.Size(416, 295);
+            this.processedImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.processedImage.TabIndex = 0;
+            this.processedImage.TabStop = false;
+            // 
+            // btnSelectPhoto
+            // 
+            this.btnSelectPhoto.Location = new System.Drawing.Point(25, 373);
+            this.btnSelectPhoto.Name = "btnSelectPhoto";
+            this.btnSelectPhoto.Size = new System.Drawing.Size(91, 38);
+            this.btnSelectPhoto.TabIndex = 2;
+            this.btnSelectPhoto.Text = "Select Photo";
+            this.btnSelectPhoto.UseVisualStyleBackColor = true;
+            this.btnSelectPhoto.Click += new System.EventHandler(this.btnSelectPhoto_Click);
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(122, 373);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(91, 38);
+            this.btnSelectFolder.TabIndex = 3;
+            this.btnSelectFolder.Text = "Select Folder of Images";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 527);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Controls.Add(this.btnSelectPhoto);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Facial Recognition Sandbox";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.originalPicture)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.processedImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox originalPicture;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox processedImage;
+        private System.Windows.Forms.Button btnSelectPhoto;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.OpenFileDialog selectImageFile;
+        private System.Windows.Forms.FolderBrowserDialog InputFolder;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using DlibDotNet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Facial_Recognition_Sandbox
+{
+    public partial class Form1 : Form
+    {
+        string Title = "Facial Recognition Sandbox";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectPhoto_Click(object sender, EventArgs e)
+        {
+            selectImageFile.Filter = "Image files (*.jpg, *.jpeg, *.bmp, *.tiff, *.png) | *.jpg; *.jpeg; *.bmp; *.tiff; *.png";
+            selectImageFile.FileName = "";
+            DialogResult result = selectImageFile.ShowDialog();
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(selectImageFile.FileName))
+            {
+                originalPicture.Image = Image.FromFile(selectImageFile.FileName);
+                ProcessImage(selectImageFile.FileName);
+            }
+        }
+
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            DialogResult result = InputFolder.ShowDialog();
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(InputFolder.SelectedPath))
+            {
+                var ext = new List<string> { ".jpg", ".gif", ".png", ".jpeg", ".tiff", ".bmp" };
+                DirectoryInfo InputFolderDir = new DirectoryInfo(InputFolder.SelectedPath);
+                foreach (var file in InputFolderDir.GetFiles("*.*", SearchOption.AllDirectories).Where(s => ext.Contains(Path.GetExtension(s.FullName).ToLowerInvariant())))
+                {
+                    ProcessImage(file.FullName);
+                }
+            }
+        }
+
+
+        void ProcessImage(string Photo)
+        {
+            try
+            {
+                using (var fd = Dlib.GetFrontalFaceDetector())
+                {
+                    var img = Dlib.LoadImage<RgbPixel>(Photo);
+
+                    // find all faces in the image
+                    var faces = fd.Operator(img);
+                    foreach (var face in faces)
+                    {
+                        Dlib.FillRect(img, face, color: new RgbPixel(204, 6, 12));
+                    }
+                    Dlib.SaveJpeg(img, "Processed" + Path.GetFileName(Photo));
+                }
+                processedImage.Image = Image.FromFile("Processed" + Path.GetFileName(Photo));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), Title);
+            }
+        }
+
+    }
+}
